7cbe8a44de487b5159321cbaeeca0f6c4e644b8b
Michael <blazeofglory999@gmail.com>
2021-09-29 17:58:38 +0300

    Added loginization by using Firebase.

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {User} from "../../shared/interfaces";
+import {AuthService} from "../shared/services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
@@ -9,21 +11,28 @@ import {User} from "../../shared/interfaces";
 })
 export class LoginPageComponent implements OnInit {
   form:FormGroup
-  constructor() {
+  submitted: boolean
+  constructor(private auth:AuthService, private router:Router) {
     this.form = new FormGroup({
       email: new FormControl('',[Validators.required,Validators.email]),
       password: new FormControl('', [Validators.required])
     })
+    this.submitted = false;
   }
 
   ngOnInit(): void {
   }
-
   submit() {
     if (this.form.invalid) return
+    this.submitted = true;
     const user: User = {
       email: this.form.value.email,
       password: this.form.value.password
     }
+    this.auth.login(user).subscribe(()=>{
+      this.form.reset();
+      this.router.navigate(['/admin','dashboard']);
+    })
+    this.submitted = false;
   }
 }
---
+++ src/app/admin/shared/services/auth.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {User} from "../../../shared/interfaces";
+import {Observable} from "rxjs";
+import {environment} from "../../../../environments/environment";
+import {tap} from "rxjs/operators";
+
+@Injectable()
+export class AuthService {
+
+  constructor(private http:HttpClient) { }
+
+  get token():string | null{
+    const expDate = new Date(`${localStorage.getItem('fb-token-exp')}`);
+    if (new Date() > expDate){
+       this.logout();
+       return null
+    }
+    return `${localStorage.getItem('fb-token')}`
+  }
+
+  login(user:User):Observable<any>{
+    user.returnSecureToken = true;
+    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
+      .pipe(
+        tap(this.setToken)
+      );
+  }
+  logout(){
+    this.setToken(null)
+  }
+  isAuthenticated(): boolean{
+    return !!this.token;
+  }
+  private setToken(response:any){
+    if (response){
+      const expDate = new Date(new Date().getTime() + +response.expiresIn * 1000);
+      localStorage.setItem('fb-token', response.idToken);
+      localStorage.setItem('fb-token-exp', expDate.toString());
+    } else {
+      localStorage.clear();
+    }}
+
+
+}
